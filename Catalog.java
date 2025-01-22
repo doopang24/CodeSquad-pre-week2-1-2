c8d8651c5a8ae33b4f4802625bb379ba0683efd5
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-22 13:57:47 +0900

    feat: Book 클래스 생성

--- Catalog.java
+++ Catalog.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
+class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
 
 public class Catalog {
 }
